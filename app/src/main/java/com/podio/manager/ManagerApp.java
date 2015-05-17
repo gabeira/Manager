55d4316015d792837c48af6a035711f83a7f917e
Gabriel Pereira <gabeira@gmail.com>
2015-05-17 11:46:35 -0400

    Initial commit

---
+++ app/src/main/java/com/podio/manager/ManagerApp.java
@@ -0,0 +1,15 @@
+package com.podio.manager;
+
+import android.app.Application;
+
+/**
+ * Created by gabrielbernardopereira on 5/17/15.
+ */
+public class ManagerApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
